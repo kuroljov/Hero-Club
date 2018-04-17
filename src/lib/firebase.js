c248a045d6a664756e64c449736484fb3c09b9a4
Viktor <viktor.konsta@gmail.com>
2018-04-18 00:27:17 +0300

    Add firebase config

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyAa_jM93NQnlMX_h38k-bptzt_XD2Gre4A',
+  authDomain: 'hero-club-d8a04.firebaseapp.com',
+  databaseURL: 'https://hero-club-d8a04.firebaseio.com',
+  projectId: 'hero-club-d8a04',
+  storageBucket: 'hero-club-d8a04.appspot.com',
+  messagingSenderId: '1057391921941'
+}
+
+export const app = firebase.initializeApp(config)
+export const db = app.database()
