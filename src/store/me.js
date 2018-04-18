34f199d859019c1cf896fc355fcd87bd72442226
Viktor <viktor.konsta@gmail.com>
2018-04-18 16:25:28 +0300

    Add wannaBattle mutation and getter to Me

--- src/store/me.js
+++ src/store/me.js
@@ -17,12 +17,15 @@ export default {
     id: (state: State) => state.me.id,
     name: (state: State) => state.me.name,
     hero: (state: State) => state.me.hero,
-    isBusy: (state: State) => state.me.isBusy,
+    wannaBattle: (state: State) => state.me.wannaBattle,
     isLoggedIn: (state: State) => !!state.me.name && !!state.me.hero
   },
   mutations: {
     addInfo (state: State, info: Object) {
       state.me = { ...state.me, ...info }
+    },
+    wannaBattle (state: State, either: boolean) {
+      state.me.wannaBattle = either
     }
   },
   actions: {
